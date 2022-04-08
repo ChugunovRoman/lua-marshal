b464b8b8be0963907edafc03f4150e4a65796f31
Chugunov Roman <chugunovrv@lad24.ru>
2022-04-08 19:35:24 +0300

    fix: Update code for cpp compatibility

--- lmarshal.h
+++ lmarshal.h
@@ -8,4 +8,13 @@
 
 #define LUA_MARSHALLIBNAME "marshal"
 
-LUALIB_API int luaopen_marshal(lua_State *L);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  LUALIB_API int luaopen_marshal(lua_State *L);
+
+#ifdef __cplusplus
+}
+#endif
